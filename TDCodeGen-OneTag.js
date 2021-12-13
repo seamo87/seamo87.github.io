20ef710ee8b6b57d381173c8a51c997c5fdd35a7
seamo87 <95230335+seamo87@users.noreply.github.com>
2021-12-13 13:21:16 +0000

    Add files via upload

--- TDCodeGen-OneTag.js
+++ TDCodeGen-OneTag.js
@@ -84,8 +84,19 @@ $(document).ready(function(){
 							}
 							else{
 					};};};};};
-	var contTags='Tags to be added on all pages of Advertiser domain\r\n=======================================================================\r\n\r\n<!-- Sales Script -->\r\n<!-- Start Tradedoubler Landing Page Tag Insert on all landing pages to handle first party cookie-->\r\n<script language=\"JavaScript\">\r\n        (function(i,s,o,g,r,a,m){i[\'TDConversionObject\']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,\'script\', \'https://svht.tradedoubler.com/tr_sdk.js\', \'tdconv\');\r\n</script>\r\n<!-- End Tradedoubler tag--> <\/script>\r\n\r\n<script type=\"text\/javascript\">\r\nvar TDConf = TDConf || {};\r\nTDConf.Config = { \r\n  pageType: \"[PAGE_TYPE]\",\r\n  pageURL: window.location.href,\r\n  orderId: \"[ORDER_NUMBER]\", \r\n  orderValue: \"[ORDER_VALUE]\",\r\n  currency: \"[CURRENCY]\",\r\n  voucher: \"[VOUCHER]\"\r\n  };\r\n<\/script>\r\n\r\n<script type=\"text\/javascript\" src=\"https:\/\/swrap.tradedoubler.com\/wrap?id='+cid+'\"><\/script>';
+	var contTags='Tags to be added on all pages of Advertiser domain\r\n=======================================================================\r\n\r\n<!-- Sales Script -->\r\n<!-- Start Tradedoubler Landing Page Tag Insert on all landing pages to handle first party cookie-->\r\n<script language=\"JavaScript\">\r\n        (function(i,s,o,g,r,a,m){i[\'TDConversionObject\']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,\'script\', \'https://svht.tradedoubler.com/tr_sdk.js\', \'tdconv\');\r\n</script>\r\n<!-- End Tradedoubler tag--> <\/script>\r\n\r\n<script type=\"text\/javascript\">\r\nvar TDConf = TDConf || {};\r\nTDConf.Config = { \r\n  pageType: \"[PAGE_TYPE]\",\r\n  pageURL: window.location.href,\r\n  orderId: \"[ORDER_NUMBER]\", \r\n  orderValue: \"[ORDER_VALUE]\",\r\n  currency: \"[CURRENCY]\",\r\n  voucher: \"[VOUCHER]\"\r\n};\r\n<\/script>\r\n\r\n<script type=\"text\/javascript\" src=\"https:\/\/swrap.tradedoubler.com\/wrap?id='+cid+'\"><\/script>';
 	var trackTags=trackPrefix+'(function(i,s,o,g,r,a,m){i[\'TDConversionObject\']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,\'script\', \'https://svht.tradedoubler.com/tr_sdk.js\', \'tdconv\');\r\n        tdconv(\'init\', \''+orgID+'\', {\'element\': \'iframe\' });\r\n        tdconv(\'track\', \'sale\',\r\n{\'transactionId\':TDConf.Config.orderId,\'ordervalue\':TDConf.Config.orderValue,\'currency\':TDConf.Config.currency,\'event\':\''+eventID+'\',\'voucher\':TDConf.Config.voucher});\r\n	}\r\n	else{\r\n	};';
+	var GTMtags='Tags to be inserted in GTM "TRADEDOUBLER COOKIE" Custom HTML tag\r\n=======================================================================\r\n\r\n<!-- Sales Script -->\r\n<!-- Start Tradedoubler Landing Page Tag Insert on all landing pages to handle first party cookie-->\r\n<script language=\"JavaScript\">\r\n(function(i,s,o,g,r,a,m){i[\'TDConversionObject\']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,\'script\', \'https://svht.tradedoubler.com/tr_sdk.js\', \'tdconv\');\r\n</script>\r\n<!-- End Tradedoubler tag--> <\/script>\r\n\r\n\r\nTags to be inserted in GTM "TRADEDOUBLER CONVERSION TAG" Custom HTML tag\r\n=======================================================================\r\n\r\n<script language=\"JavaScript\">\r\n        (function(i,s,o,g,r,a,m){i[\'TDConversionObject\']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,\'script\', \'https://svht.tradedoubler.com/tr_sdk.js\', \'tdconv\');\r\n        tdconv(\'init\', \''+orgID+'\', {\'element\': \'iframe\' });\r\n        tdconv(\'track\', \'sale\',\r\n{\'transactionId\':\'[ORDER_NUMBER]\',\'ordervalue\':\'[ORDER_VALUE]\',\'currency\':\'[CURRENCY]\',\'event\':\''+eventID+'\',\'voucher\':\'[VOUCHER_CODE]\'});\r\n<\/script>';
+	var shopifyTags='Tags to be added in Shopify under Settings -> Checkout -> Order Processing -> Order Status text box\r\n=======================================================================\r\n\r\n<script language=\"JavaScript\">\r\n(function(i,s,o,g,r,a,m){i[\'TDConversionObject\']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,\'script\', \'https://svht.tradedoubler.com/tr_sdk.js?\' + Math.random(), \'tdconv\');\r\ntdconv(\'init\', \''+orgID+'\', {\'program\':false});\r\n;\r\ntdconv(\"track\", \"sale\", {\"transactionId\":\"{{ order_number }}\",\"ordervalue\":\"{{ td_order_value }}\",\"currency\":\"{{ currency }}\",\"event\":\"'+eventID+'\", \"voucher\":\"{{ discounts.first.code }}\"});\r\n<\/script>';
+	if($('#checkReport').is(':checked'))
+	{contTags='Tags to be added on all pages of Advertiser domain\r\n=======================================================================\r\n\r\n<!-- Sales Script -->\r\n<!-- Start Tradedoubler Landing Page Tag Insert on all landing pages to handle first party cookie-->\r\n<script language=\"JavaScript\">\r\n        (function(i,s,o,g,r,a,m){i[\'TDConversionObject\']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,\'script\', \'https://svht.tradedoubler.com/tr_sdk.js\', \'tdconv\');\r\n</script>\r\n<!-- End Tradedoubler tag--> <\/script>\r\n\r\n<script type=\"text\/javascript\">\r\nvar TDConf = TDConf || {};\r\nTDConf.Config = { \r\n  pageType: \"[PAGE_TYPE]\",\r\n  pageURL: window.location.href,\r\n  orderId: \"[ORDER_NUMBER]\", \r\n  orderValue: \"[ORDER_VALUE]\",\r\n  currency: \"[CURRENCY]\",\r\n  voucher: \"[VOUCHER]\"\r\n  reportInfo:[\r\n		f1:\'[PRODUCT IDs]\', f2:\'[PRODUCT NAMES]\',f3:\'[PRODUCT VALUES]\',f4:\'[QUANTITY]\'\r\n	];\r\n<\/script>\r\n\r\n<script type=\"text\/javascript\" src=\"https:\/\/swrap.tradedoubler.com\/wrap?id='+cid+'\"><\/script>';
+	trackTags=trackPrefix+'(function(i,s,o,g,r,a,m){i[\'TDConversionObject\']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,\'script\', \'https://svht.tradedoubler.com/tr_sdk.js\', \'tdconv\');\r\n        tdconv(\'init\', \''+orgID+'\', {\'element\': \'iframe\' });\r\n        tdconv(\'track\', \'sale\',\r\n{\'transactionId\':TDConf.Config.orderId,\'ordervalue\':TDConf.Config.orderValue,\'currency\':TDConf.Config.currency,\'event\':\''+eventID+'\',\'voucher\':TDConf.Config.voucher,\'reportInfo\':TDConf.Config.reportInfo});\r\n	}\r\n	else{\r\n	};';
+	GTMtags='Tags to be inserted in GTM "TRADEDOUBLER COOKIE" Custom HTML tag\r\n=======================================================================\r\n\r\n<!-- Sales Script -->\r\n<!-- Start Tradedoubler Landing Page Tag Insert on all landing pages to handle first party cookie-->\r\n<script language=\"JavaScript\">\r\n(function(i,s,o,g,r,a,m){i[\'TDConversionObject\']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,\'script\', \'https://svht.tradedoubler.com/tr_sdk.js\', \'tdconv\');\r\n</script>\r\n<!-- End Tradedoubler tag--> <\/script>\r\n\r\n\r\nTags to be inserted in GTM "TRADEDOUBLER CONVERSION TAG" Custom HTML tag\r\n=======================================================================\r\n\r\n<script language=\"JavaScript\">\r\n        (function(i,s,o,g,r,a,m){i[\'TDConversionObject\']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,\'script\', \'https://svht.tradedoubler.com/tr_sdk.js\', \'tdconv\');\r\n        tdconv(\'init\', \''+orgID+'\', {\'element\': \'iframe\' });\r\n        tdconv(\'track\', \'sale\',\r\n{\'transactionId\':\'[ORDER_NUMBER]\',\'ordervalue\':\'[ORDER_VALUE]\',\'currency\':\'[CURRENCY]\',\'event\':\''+eventID+'\',\'voucher\':\'[VOUCHER_CODE]\',\'reportInfo\':\'[reportInfo]\'});\r\n<\/script>';
+	shopifyTags='Tags to be added in Shopify under Settings -> Checkout -> Order Processing -> Order Status text box\r\n=======================================================================\r\n\r\n<script language=\"JavaScript\">\r\n(function(i,s,o,g,r,a,m){i[\'TDConversionObject\']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,\'script\', \'https://svht.tradedoubler.com/tr_sdk.js?\' + Math.random(), \'tdconv\');\r\ntdconv(\'init\', \''+orgID+'\', {\'program\':false});\r\nvar reportInfo =\'\';\r\n{% assign td_order_value = subtotal_price | minus: tax_price | money_without_currency | remove: \'.\' | remove: \',\' | divided_by: 100.0 %}\r\n{% for basket in line_items %}\r\nreportInfo = reportInfo + \"f1={{ basket.sku }}&f2={{ basket.title }}&f3={{ basket.price | money_without_currency | remove: \'.\' | remove: \',\' | divided_by: 100.0 }}&f4={{ basket.quantity }}|\";\r\n{% endfor %}\r\nreportInfo = encodeURIComponent(reportInfo.slice(0,-1));\r\ntdconv(\"track\", \"sale\", {\"transactionId\":\"{{ order_number }}\",\"ordervalue\":\"{{ td_order_value }}\",\"currency\":\"{{ currency }}\",\"event\":\"'+eventID+'\", \"voucher\":\"{{ discounts.first.code }}\", \"reportInfo\":reportInfo});\r\n<\/script>';
+	}
+	
+	else{}
+	
 	e.preventDefault();
 	   if($('#container').is(':checked'))
 	   {
@@ -105,9 +116,28 @@ $(document).ready(function(){
 				return;
 			}
 			else{
+				if($('#GTM').is(':checked'))
+				{
+					$('#tags').show();
+					$('#tagCode').val('');
+					$('#tagCode').val( $('#tagCode').val() + GTMtags);
+					txtname = 'TD GTM tags - '+orgID+'.txt';
+					return;
+				}
+				else{
+					if($('#shopify').is(':checked'))
+					{
+					$('#tags').show();
+					$('#tagCode').val('');
+					$('#tagCode').val( $('#tagCode').val() + shopifyTags);
+					txtname = 'TD Shopify tags - '+orgID+'.txt';
+					return;
+					}
+					else{
+					}
 			};
 		};
-	});
+	};
 	const downloadToFile = (content, filename, contentType) => {
 	const a = document.createElement('a');
 	const file = new Blob([content], {type: contentType});
@@ -124,4 +154,4 @@ $(document).ready(function(){
 	downloadToFile(txt.value, txtname, 'text/plain');
 	});
 	});
- 
+				});
